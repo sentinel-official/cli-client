49fe44fd39c82fb83d19b3f8fba53f12b67beede
Srinivas Baride <srini@exidio.co>
2021-08-12 18:45:13 +0530

    Removed unnecessary code in main file

--- go.mod
+++ go.mod
@@ -8,12 +8,13 @@ require (
 	github.com/cosmos/go-bip39 v1.0.0
 	github.com/go-kit/kit v0.10.0
 	github.com/gorilla/mux v1.8.0
+	github.com/natefinch/atomic v1.0.1
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/pkg/errors v0.9.1
 	github.com/rs/cors v1.7.0
 	github.com/sentinel-official/hub v0.7.0
 	github.com/spf13/cobra v1.1.3
-	github.com/spf13/viper v1.8.0
+	github.com/tendermint/tendermint v0.34.10
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
 )
 
--- main.go
+++ main.go
@@ -2,61 +2,34 @@ package main
 
 import (
 	"context"
-	"os"
-	"path/filepath"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/client/keys"
 	"github.com/cosmos/cosmos-sdk/version"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/sentinel-official/hub"
 	hubtypes "github.com/sentinel-official/hub/types"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 
 	"github.com/sentinel-official/cli-client/cmd"
-	"github.com/sentinel-official/cli-client/types"
-	configtypes "github.com/sentinel-official/cli-client/types/config"
 )
 
 func main() {
 	hubtypes.GetConfig().Seal()
+
 	root := &cobra.Command{
-		Use:          "sentinelcli",
-		SilenceUsage: true,
+		Use:                        "sentinelcli",
+		SilenceUsage:               true,
+		SuggestionsMinimumDistance: 2,
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			var (
-				home       = viper.GetString(flags.FlagHome)
-				configPath = filepath.Join(home, "config.toml")
-			)
-
-			if _, err := os.Stat(configPath); err != nil {
-				if err := os.MkdirAll(home, 0700); err != nil {
-					return err
-				}
-
-				config := configtypes.NewConfig().
-					WithDefaultValues()
-				if err := config.SaveToPath(configPath); err != nil {
-					return err
-				}
-			}
-
-			var (
-				config    = hub.MakeEncodingConfig()
-				clientCtx = client.Context{}.
+				config = hub.MakeEncodingConfig()
+				ctx    = client.Context{}.
 					WithJSONMarshaler(config.Marshaler).
 					WithInterfaceRegistry(config.InterfaceRegistry).
 					WithTxConfig(config.TxConfig).
-					WithLegacyAmino(config.Amino).
-					WithInput(os.Stdin).
-					WithAccountRetriever(authtypes.AccountRetriever{}).
-					WithBroadcastMode(flags.BroadcastBlock).
-					WithHomeDir(types.DefaultHomeDirectory)
+					WithLegacyAmino(config.Amino)
 			)
 
-			if err := client.SetCmdClientContextHandler(clientCtx, cmd); err != nil {
+			if err := client.SetCmdClientContextHandler(ctx, cmd); err != nil {
 				return err
 			}
 
@@ -67,15 +40,12 @@ func main() {
 	root.AddCommand(
 		cmd.ConnectCmd(),
 		cmd.DisconnectCmd(),
+		cmd.KeysCmd(),
 		cmd.QueryCommand(),
 		cmd.StartCmd(),
 		cmd.TxCommand(),
-		keys.Commands(types.DefaultHomeDirectory),
 		version.NewVersionCommand(),
 	)
-
-	root.PersistentFlags().String(flags.FlagHome, types.DefaultHomeDirectory, "home directory of the application")
-	_ = viper.BindPFlag(flags.FlagHome, root.PersistentFlags().Lookup(flags.FlagHome))
 
 	_ = root.ExecuteContext(
 		context.WithValue(
